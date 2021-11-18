ef628841bb7d86d8e273cdc6c15bb903d85218cb
Sheila Kerber <sheila.skerber@gmail.com>
2021-11-18 16:23:19 -0300

    solves sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from 'react';
 
-import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
 
-// import { SideBar } from './components/SideBar';
+import { SideBar } from './components/SideBar';
 // import { Content } from './components/Content';
 
 import { api } from './services/api';
@@ -13,7 +12,7 @@ import './styles/global.scss';
 import './styles/sidebar.scss';
 import './styles/content.scss';
 
-interface GenreResponseProps {
+export interface GenreResponseProps {
   id: number;
   name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
   title: string;
@@ -31,12 +30,10 @@ interface MovieProps {
 }
 
 export function App() {
-  const [selectedGenreId, setSelectedGenreId] = useState(1);
-
+  const [selectedGenreId, setSelectedGenreId] = useState<number>(1);
   const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-
   const [movies, setMovies] = useState<MovieProps[]>([]);
-  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps); 
 
   useEffect(() => {
     api.get<GenreResponseProps[]>('genres').then(response => {
@@ -60,22 +57,8 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
+      
+      <SideBar genres={genres} selectedGenreId={selectedGenreId} handleClickButton={handleClickButton} />
 
       <div className="container">
         <header>
